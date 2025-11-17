a8cdb96357c82aae404d631ef722f8813c53e051
LucianoHP <lucianohuallparimachi04@gmail.com>
2025-11-17 11:01:10 -0500

    Agregando estructura base de Node.js Backend y Frontend

---
+++ src/controllers/userController.js
@@ -0,0 +1,10 @@
+import { getAllUsers } from "../services/userService.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener usuarios" });
+  }
+};
